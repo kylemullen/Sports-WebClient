e4674a441760dc511f7218ca1ec1fa0e50119f0c
“kylemullengit config --global user.email “kylem.mullen.com <kylem.mullen@gmail.com>
2014-11-18 20:10:37 -0600

    Added .env file to root folder to accomodate different versions of the API. Changed model methods to access the .env

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -17,13 +17,13 @@ class Sport
 	end
 
 	def self.find(id)
-		return Sport.new(Unirest.get("http://localhost:3000/sports/#{id}.json").body)
+		return Sport.new(Unirest.get("#{ENV["SPORT_API_ROOT"]}/sports/#{id}.json").body)
 
 	end
 
 	def self.all
 		@sports = []
-		  raw_sports = Unirest.get(("http://localhost:3000/sports.json")).body
+		  raw_sports = Unirest.get("#{ENV["SPORT_API_ROOT"]}/sports.json").body
 	  	  raw_sports.each do |sport_hash|
 	  	    @sports << Sport.new(sport_hash)
 	  	end
@@ -33,15 +33,12 @@ class Sport
 
 	def self.create
 
-
-
-
 	end 
 
-	def destroy
-		Unirest.delete("http://localhost:3000/sports/#{@id]}.json", headers:{ "Accept" => "application/json"})
+	# def destroy
+	# 	Unirest.destroy("http://localhost:3000/sports/#{@id]}.json", headers:{ "Accept" => "application/json"})
 
-	end
+	# end
 end
 
 
